fdf4fc19922f537f97717c8b95959411e5d77067
bonatov <Vincent.Bonato@etu.univ-grenoble-alpes.fr>
2022-02-27 18:17:51 +0100

    Exo6, but sayHello2 cmpt don't works

--- src/Info_itf.java
+++ src/Info_itf.java
@@ -1,5 +1,6 @@
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Info_itf {
+public interface Info_itf extends Remote {
     public String getName() throws RemoteException;
 }
